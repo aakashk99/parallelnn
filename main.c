4cc6558f1e11454cb26b93b1851d61fb45c6de3e
Aakash Karlekar <ak@macbook-pro.resource.campus.njit.edu>
2024-09-04 17:49:30 -0400

    Fix swap file issue

--- main.c
+++ main.c
@@ -10,41 +10,40 @@
 #include "matrix/ops.h"
 
 int main() {
-	//TIMING
-	srand(time(NULL));
-	cudaEvent_t start, stop;
-    	cudaEventCreate(&start);
-    	cudaEventCreate(&stop);
-    	float elapsedTime;
+        //TIMING
+        srand(time(NULL));
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        float elapsedTime;
 
-	//TRAINING
-	cudaEventRecord(start, 0);
-	int number_imgs = 10000;
-	Img** imgs = csv_to_imgs("./data/mnist_train.csv", number_imgs);
-	NeuralNetwork* net = network_create(0.1, 3, 784, 300, 10);
-	network_train_batch_imgs(net, imgs, number_imgs);
-	network_save(net, "testing_net");
+        //TRAINING
+        cudaEventRecord(start, 0);
+        int number_imgs = 10000;
+        Img** imgs = csv_to_imgs("./data/mnist_train.csv", number_imgs);
+        NeuralNetwork* net = network_create(0.1, 3, 784, 300, 10);
+        network_train_batch_imgs(net, imgs, number_imgs);
+        network_save(net, "testing_net");
 
-	cudaEventRecord(stop, 0);
-    	cudaEventSynchronize(stop);
-    	cudaEventElapsedTime(&elapsedTime, start, stop);
-    	printf("Runtime: %3.1f ms\n", elapsedTime);
+        cudaEventRecord(stop, 0);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsedTime, start, stop);
+        printf("Runtime: %3.1f ms\n", elapsedTime);
 
-	// PREDICTING
-//	cudaEventRecord(start, 0);
-//	int number_imgs = 3000;
-//	Img** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
-//	NeuralNetwork* net = network_load("testing_net");
-//	double score = network_predict_imgs(net, imgs, number_imgs);
-//	printf("Score: %1.5f\n", score);
-//	cudaEventRecord(stop, 0);
-//    	cudaEventSynchronize(stop);
-//    	cudaEventElapsedTime(&elapsedTime, start, stop);
-//    	printf("Runtime: %3.1f ms\n", elapsedTime);
+        // PREDICTING
+//      cudaEventRecord(start, 0);
+//      int number_imgs = 3000;
+//      Img** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
+//      NeuralNetwork* net = network_load("testing_net");
+//      double score = network_predict_imgs(net, imgs, number_imgs);
+//      printf("Score: %1.5f\n", score);
+//      cudaEventRecord(stop, 0);
+//      cudaEventSynchronize(stop);
+//      cudaEventElapsedTime(&elapsedTime, start, stop);
+//      printf("Runtime: %3.1f ms\n", elapsedTime);
 //
-//	imgs_free(imgs, number_imgs);
-//	network_free(net);
+//      imgs_free(imgs, number_imgs);
+//      network_free(net);
     
-	return 0;
+        return 0;
 }
-
